be39714d46c4fef10bf676cad7d97ad18deceaa8
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-03-18 16:47:57 +0600

    Парсинг всех страниц новостей

--- Parser.py
+++ Parser.py
@@ -2,17 +2,46 @@ from bs4 import BeautifulSoup  # импортируем библиотеку Bea
 import requests  # импортируем библиотеку requests
 
 
-def parse():
-    news = []
-    url = 'https://omgtu.ru/l/?PAGEN_1=1'  # передаем необходимы URL адрес
-    page = requests.get(url)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
-    print("STATUS CODE:", page.status_code)  # смотрим ответ
-    soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
+def parse() -> list:
+    headings = []  # список для хранения заголовков
+    page_counter = 1  # счётчик страниц
+    news_amount = parse_amount()  # находим количество статей
 
-    block = soup.findAll('h3', class_='news-card__title')  # находим  контейнер с нужным классом
-    for data in block:  # проходим циклом по содержимому контейнера
-        news.append(normalize(data.text))
+    while (len(headings) < news_amount):  # перебираем все страницы
+        url = 'http://omgtu.ru/l/?PAGEN_1=' + str(page_counter)  # передаем URL-адрес сайта Политеха + нужную страницу
+        page = requests.get(url)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+        if page.status_code != 200:  # проверка на доступность omgtu.ru
+            print("ОШИБКА! Не удалось подключиться к omgtu.ru")
+            exit(1)
+        soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
+        block = soup.findAll('h3', class_='news-card__title')  # находим  контейнер с нужным классом
+        for data in block:  # проходим циклом по содержимому контейнера
+            text = normalize(data.text)
+            headings.append(text)  # добавляем обработанный текст заголовка в список
+        page_counter += 1  # переходим к следующей странице
+
+    print("СТРАНИЦ:", page_counter - 1)
+    print("ОБРАБОТАНО ЗАГОЛОВКОВ:", len(headings))
+    return headings
 
 
-def normalize(text: str):  # преобразуем строку в нормальный вид
+def normalize(text: str) -> str:  # преобразуем строку в нормальный вид
     return text.replace('\n', '').strip(' ') + '\n'  # убираем лишние пробелы, перенос строки ставим в конце
+
+
+def parse_amount() -> int:
+    amount = 0
+    url = 'http://omgtu.ru/l/'  # передаём URL сайта Политеха
+    page = requests.get(url)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    if page.status_code != 200:  # проверка на доступность omgtu.ru
+        print("ОШИБКА! Не удалось подключиться к omgtu.ru")
+        exit(1)
+    soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
+    block = soup.findAll('font', class_='text')  # находим  контейнер с нужным классом
+    for data in block:
+        pointer = data.text.find("из")  # ищем кодовое слово 'из' (страница xxx из xxx )
+        if pointer == -1:  # если в текущем блоке не найдено, идём к следующему
+            break
+        amount = int(data.text[pointer + 3::])  # делаем срез и конвертируем в число
+    print("НАЙДЕНО СТАТЕЙ:", amount)
+    return amount
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import Parser
+from Parser import parse
+from datetime import datetime  # импортируем библиотеку для работу с датой и временем
 
 if __name__ == '__main__':
-    Parser.parse()
+    headings_formatted = ''.join(parse())  # превращаем список в строку с нулевым разделителем
+    with open('OUTPUT.txt', 'w', encoding='utf-8') as F:  # открываем файл на запись
+        F.write(datetime.now().strftime("%d.%m.%Y %H:%M") + '\n' + headings_formatted)  # доп. добавил текущ. дату
+    print("РЕЗУЛЬТАТ СОХРАНЁН В OUTPUT.txt")
