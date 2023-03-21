2881f5a08295c7dc0df3c9b65089c87ac78cafce
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-03-21 13:46:30 +0600

    Поправил табуляцию

--- Saver.py
+++ Saver.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def save(lst: list):
 headings_formatted = '\n'.join(lst)  # превращаем список в строку с построчным разделителем
-    with open('OUTPUT.txt', 'w', encoding='utf-8') as F:  # открываем файл на запись
-        F.write(datetime.now().strftime("%d.%m.%Y %H:%M") + '\n' + headings_formatted)  # доп. добавил текущ. дату
-    print("РЕЗУЛЬТАТ СОХРАНЁН В OUTPUT.txt")
+with open('OUTPUT.txt', 'w', encoding='utf-8') as F:  # открываем файл на запись
+    F.write(datetime.now().strftime("%d.%m.%Y %H:%M") + '\n' + headings_formatted)  # доп. добавил текущ. дату
+print("РЕЗУЛЬТАТ СОХРАНЁН В OUTPUT.txt")
