ea5f131a0ac88b9b01b53fc901febd2444d04059
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-03-21 13:42:30 +0600

    Поправил main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Parser import parse  # импортируем основную функцию parse() из Parser
+from Saver import save
 from datetime import datetime  # импортируем библиотеку для работы с датой и временем
 
 if __name__ == '__main__':
-    headings_formatted = '\n'.join(parse())  # превращаем список в строку с построчным разделителем
-    with open('OUTPUT.txt', 'w', encoding='utf-8') as F:  # открываем файл на запись
-        F.write(datetime.now().strftime("%d.%m.%Y %H:%M") + '\n' + headings_formatted)  # доп. добавил текущ. дату
-    print("РЕЗУЛЬТАТ СОХРАНЁН В OUTPUT.txt")
+    save(parser())
